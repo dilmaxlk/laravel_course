3da4a823c061b6e973801e33baefa2a70c0d06c1
DilMAX-IT <dilamxit@gmail.com>
2023-11-12 01:04:10 +0530

    24-BladeDirective-if-statement

---
+++ 24-BladeDirective-if-statement/views/home.blade.php
@@ -0,0 +1,45 @@
+<?php
+// if($country == 'SriLanka'){
+//     echo 'This is Sri Lanka';
+// }else{
+//     echo 'No Country';
+// }
+?>
+
+
+{{-- @if($country == 'SriLanka')
+    This is SriLanka
+@elseif($country == 'India')
+    This is India
+@else
+    Another country that is not SriLanka and India
+@endif --}}
+
+{{-- ------------------------------------------------------------------------- --}}
+
+<?php
+// if(isset($city)){
+//     echo 'This is Kalutara';
+// }
+
+?>
+
+{{-- @isset($city)
+    There is city
+@endisset --}}
+
+
+{{-- ---------------------------------------------------------------------------}}
+
+<?php
+// if(empty($city)){
+//     echo 'This is Kalutara';
+// }
+?>
+
+{{-- @empty($city)
+    Somethings
+@endempty --}}
+
+
+

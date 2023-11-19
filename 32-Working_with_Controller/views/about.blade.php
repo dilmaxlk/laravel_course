458515c17ebe15c882e7146c19e5bfb4bbeb6404
DilMAX-IT <dilamxit@gmail.com>
2023-11-19 23:56:29 +0530

    32-Working_with_Controller

---
+++ 32-Working_with_Controller/views/about.blade.php
@@ -0,0 +1,3 @@
+<h1>About Page</h1>
+<p>About Page Content</p>
+<h3>Username is: {{$username}}</h3>
---
+++ 32-Working_with_Controller/views/home.blade.php
@@ -0,0 +1,13 @@
+<h1>Home Page</h1>
+<p>Home Page Content</p>
+<h3>Hello {{$name}}</h3>
+
+
+
+
+{{-- 
+
+This is comment.
+This is new line. 
+
+--}}

bfd6aee0a1d16e1add3a89e48137cee7f516c271
DilMAX-IT <dilamxit@gmail.com>
2024-02-07 23:49:25 +0530

    78-79-RawSQLQuery-Update-DeleteData

---
+++ 78-79-RawSQLQuery-Update-DeleteData/app/Http/Controllers/StudentController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class StudentController extends Controller
+{
+    public function index()
+    {
+        // $all = DB::select('select * from students');
+        // foreach($all as $single){
+        //     echo $single->name.'<br>';
+        //     echo $single->email.'<br><br><br>';
+        // }
+
+        //Show any single data
+        $single = DB::select('select * from students where id=?', [1]);
+        echo $single[0]->name.'<br>';
+        echo $single[0]->email;
+    }
+
+
+    public function create()
+    {
+        DB::insert('INSERT INTO students (name, email) VALUES (?, ?)', ['Nimal', 'nimal@example.com']);
+
+    }
+
+    public function update()
+    {
+        DB::update('update students set name=? where id=?', ['Kamal', 1]);
+    }
+
+    public function delete()
+    {
+        DB::delete('delete from students where id=?', [4]);
+    }
+}

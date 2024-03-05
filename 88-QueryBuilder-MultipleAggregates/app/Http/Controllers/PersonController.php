59bc9afdd9eaa956ce761cd942fe2288989ed707
DilMAX-IT <dilamxit@gmail.com>
2024-03-06 00:35:05 +0530

    88-QueryBuilder-MultipleAggregates

---
+++ 88-QueryBuilder-MultipleAggregates/app/Http/Controllers/PersonController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class PersonController extends Controller
+{
+    public function index()
+    {
+        $result = DB::table('persons')
+        ->select('country', 
+            DB::raw('COUNT(*) as total_count'),
+            DB::raw('AVG(age) as avg_age'),
+            DB::raw('MAX(salary) as max_salary')
+            )
+        ->groupBy('country')
+        ->get();
+
+
+        foreach($result as $item){
+            $country = $item->country;
+            $count = $item->total_count;
+            $avg_age = $item->avg_age;
+            $max_salary = $item->max_salary;
+            
+            echo 'Country:'.$country.'<br>';
+            echo 'Total Persons:'.$count.'<br>';
+            echo 'Avg Age:'.$avg_age.'<br>';
+            echo 'MAX Salary:'.$max_salary.'<br><br><br>';
+        }
+
+
+        // $result = DB::table('persons')
+        // ->select('country', DB::raw('COUNT(*) as total_count'))
+        // ->groupBy('country')
+        // ->get();
+
+
+        // foreach($result as $item){
+        //     $country = $item->country;
+        //     $count = $item->total_count;
+        //     echo 'Country:'.$country.'<br>';
+        //     echo 'Total Persons:'.$count.'<br><br><br>';
+        // }
+
+
+       //echo DB::table('persons')->count();
+       //echo DB::table('persons')->sum('salary');
+       //echo DB::table('persons')->avg('salary');
+       //echo DB::table('persons')->max('age');
+       //echo DB::table('persons')->min('age');  
+    }
+}

f4b34e6585dd0da3e00530fc34d128d8729a84fb
MAXMIND-Education <maxmindeducation@gmail.com>
2024-04-11 00:37:25 +0530

    102-EloquentORM-firstOrCreate-firstOrNew

---
+++ 102-EloquentORM-firstOrCreate-firstOrNew/web.php
@@ -0,0 +1,27 @@
+<?php
+//97)   Eloquent ORM - Insert Data
+
+use App\Http\Controllers\StudentController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('student/create', [StudentController::class, 'create']);
+Route::get('student/show', [StudentController::class, 'show']);
+Route::get('student/update', [StudentController::class, 'update']);
+Route::get('student/delete', [StudentController::class, 'delete']);
+
+Route::get('student/index', [StudentController::class, 'index']);

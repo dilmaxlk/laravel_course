07e73643ad9b6600d9cd87efd6b28b39bcd03b74
MAXMIND-Education <maxmindeducation@gmail.com>
2024-04-16 01:04:25 +0530

    105-EQRelationship-OnetoOne-Inverse

---
+++ 105-EQRelationship-OnetoOne-Inverse/StudentController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use App\Models\StudentProfile;
+use Illuminate\Http\Request;
+
+
+class StudentController extends Controller
+{
+    public function index()
+    {
+        // $single = Student::with('profile')->find(2);
+        // echo $single->name.'<br>';  
+        // echo $single->email.'<br>';  
+        // echo $single->profile->country.'<br>'; 
+
+        $single = StudentProfile::with('student')->find(2);
+        echo $single->student->name.'<br>';
+        echo $single->student->email.'<br>'; 
+        echo $single->country;       
+    }
+}
